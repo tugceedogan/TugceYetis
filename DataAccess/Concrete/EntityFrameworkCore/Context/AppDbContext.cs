ce1bbdf20f5ad512573d2b35922d23eedc5e2d33
Tuğçe Doğan <tugceedogann3@gmail.com>
2021-06-15 10:51:37 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFrameworkCore/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.EntityFrameworkCore.Context
+{
+    public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS; Initial Catalog=afetgiris; Integrated Security=True; Persist Security Info=False");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+
+
+
+    }
+}
---
+++ Entities/Concrete/location.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class location: Core.Entities.IEntity
+    {
+        [Key]
+        public int location_id { get; set; } //lokasyon id
+        public string title { get; set; } //lokasyon başlık
+        public string description { get; set; } // lokasyon açıklama
+        public int image_id { get; set; } //lokasyon resim////////////////////////////////////////////
+        public bool? state { get; set; } //lokasyon görünme durumu
+        public bool? IsChecked { get; set; } //lokasyon silinip silinmediği
+
+
+
+    }
+}
